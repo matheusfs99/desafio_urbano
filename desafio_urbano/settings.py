5abc06d0d9e16efc6249ece4aab38f9931c48731
Matheus Farias <matheusfarias009@hotmail.com>
2021-06-07 11:49:29 -0300

    criação das apps

--- desafio_urbano/settings.py
+++ desafio_urbano/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'desafio_urbano.processo',
+    'desafio_urbano.cadastro_processo',
 ]
 
 MIDDLEWARE = [
